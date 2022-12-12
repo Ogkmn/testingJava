10a323deccd219e66ec0ca38c9e3700ed1db8007
Onur Gokmen <Onur.Gokmen@rabobank.nl>
2022-12-12 13:55:27 +0100

    parameterized test

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,15 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @ParameterizedTest
-    @CsvSource({"4,5","2,3","1,1"})
-    void testMultiplyGoodCase(double x, double y){
-        assertEquals(x*y,Calculator.multiply(x,y));
+    @CsvSource({"4,5,20","2,3,6","1,1,1"})
+    void testMultiplyGoodCase(double x, double y,double expected){
+        assertEquals(Calculator.multiply(x,y),expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"5.0,1.0","100.0,2.0","33.0,9.0"})
-    void testDivideGoodCase(double x, double y){
-        assertEquals(x/y,Calculator.divide(x,y));
+    @CsvSource({"5.0,1.0,5","100.0,2.0,50.0","27.0,9.0,3"})
+    void testDivideGoodCase(double x, double y,double expected){
+        assertEquals(Calculator.divide(x,y),expected);
     }
     @Test
     void testDivideDivideBy0() throws ArithmeticException{
@@ -27,18 +27,14 @@ class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"5.0,1","100,2","33,9"})
-    void testSumGoodCase(double x, double y){
-        assertEquals(x+y,Calculator.sum(x,y));
+    @CsvSource({"5.0,1,6","100,2,102","33,9,42"})
+    void testSumGoodCase(double x, double y,double expected){
+        assertEquals(Calculator.sum(x,y),expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"5,1","100,2","33,9"})
-    void testSubtractGoodCase(double x, double y){
-        assertEquals(x-y,Calculator.subtract(x,y));
+    @CsvSource({"5,1,4","100,2,98","33,9,24"})
+    void testSubtractGoodCase(double x, double y,double expected){
+        assertEquals(Calculator.subtract(x,y),expected);
     }
-
-
-
-
 }
